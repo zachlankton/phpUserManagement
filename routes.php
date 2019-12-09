2fd22c5c04fc07b3937770c18a3bd8c5b8eceb0d
zachlankton <zachlankton@gmail.com>
2019-12-08 20:06:26 -0500

    Update routes.php

--- routes.php
+++ routes.php
@@ -3,22 +3,34 @@
 	global $account;
 
 	if ($account->isSuper()){
+		// SUPER USER ROUTES
 		require "../../super_user_db.php";
+		switch ($uri) {
+			case "/adduser":
+				require "../routes/adduser.php";
+				die();
+			case "/getuser":
+				require "../routes/getuser.php";
+				die();
+		}
 	}else{
 		require "../../regular_user_db.php";
 	}
 
+
+	// REGULAR USER ROUTES
 	switch ($uri) {
 		case "/asdf":
 			require "../routes/asdf.php";
 			die();
-		case "/adduser":
-			require "../routes/adduser.php";
-			die();
-		case "/getuser":
-			require "../routes/getuser.php";
+		case "/couch":
+			require "../routes/couch.php";
 			die();
 	}
+
+
+
+	
 
 	header("Location: /404.html");
 
