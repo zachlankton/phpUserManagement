a7bbd8677bf5fa248dd601a3fbc45909f66345d8
zachlankton <zachlankton@gmail.com>
2019-12-08 20:57:34 -0500

    Update couch.php

--- routes/couch.php
+++ routes/couch.php
@@ -1,22 +1,31 @@
 <?php
   $req = $_SERVER['REQUEST_METHOD'];
   $uri = substr($_SERVER['REQUEST_URI'], 6);
-  //echo $req;
-  //echo $uri;
+  
   // create curl resource
-        $ch = curl_init();
+  $ch = curl_init();
 
-        // set url
-        curl_setopt($ch, CURLOPT_URL, "http://127.0.0.1:5984/");
+  // set url
+  curl_setopt($ch, CURLOPT_URL, "http://127.0.0.1:5984/".$uri);
 
-        //return the transfer as a string
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  //return the transfer as a string
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-        // $output contains the output string
-        $output = curl_exec($ch);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $req);
 
-        // close curl resource to free up system resources
-        curl_close($ch); 
+  if ($req = "POST"){
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST['data']); 
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+      'Content-Type: application/json',                                                                                
+      'Content-Length: ' . strlen($data_string))                                                                       
+    );    
+  }
 
-        echo $output;
+  // $output contains the output string
+  $output = curl_exec($ch);
+
+  // close curl resource to free up system resources
+  curl_close($ch); 
+
+  echo $output;
 ?>
