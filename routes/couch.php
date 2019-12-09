8c1a403cfadbeb2dfc4a7ee2f6347f5af54080af
zachlankton <zachlankton@gmail.com>
2019-12-08 20:35:12 -0500

    Update couch.php

--- routes/couch.php
+++ routes/couch.php
@@ -3,5 +3,20 @@
   $uri = substr($_SERVER['REQUEST_URI'], 6);
   //echo $req;
   //echo $uri;
-  phpinfo();
+  // create curl resource
+        $ch = curl_init();
+
+        // set url
+        curl_setopt($ch, CURLOPT_URL, "http://127.0.0.1:5984/");
+
+        //return the transfer as a string
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        // $output contains the output string
+        $output = curl_exec($ch);
+
+        // close curl resource to free up system resources
+        curl_close($ch); 
+
+        echo $output;
 ?>
