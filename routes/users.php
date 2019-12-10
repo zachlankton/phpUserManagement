b4de2e5120ef46efc29d9848a4bac1606051c717
zachlankton <zachlankton@gmail.com>
2019-12-09 20:30:34 -0500

    Update users.php

--- routes/users.php
+++ routes/users.php
@@ -1,10 +1,12 @@
 <?php
   global $pdo;
   
-  $sth = $pdo->prepare("SELECT * FROM accounts");
+  $sth = $pdo->prepare("SELECT account_id, account_name, account_reg_time, account_enabled, super_user FROM accounts");
   $sth->execute();
 
   /* Fetch all of the remaining rows in the result set */
   $result = $sth->fetchAll();
-  var_dump($result);
+
+  header('Content-Type: application/json');
+  echo json_encode($result);
 ?>
