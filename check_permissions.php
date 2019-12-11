01359f65274ff02fc3b6c262b8c09581f8da87a4
zachlankton <zachlankton@gmail.com>
2019-12-10 21:36:16 -0500

    Update check_permissions.php

--- check_permissions.php
+++ check_permissions.php
@@ -2,12 +2,23 @@
 
   global $pdo;
   global $account;
+  global $uri;
 
   $user = $account->getName();
   $roles = $account->getUserRoles();
+  $req_type = $_SERVER['REQUEST_METHOD'];
   
+  $sth = $pdo->prepare("
+    SELECT * FROM Application.`route_permissions` 
+    WHERE request_type = :req_type AND :uri RLIKE route_regexp ");
+  $sth->execute( array(':req_type' => $req_type, ':uri' => $uri) );
+  /* Fetch all of the remaining rows in the result set */
+  $routes = $sth->fetchAll(PDO::FETCH_ASSOC);
+  
+  echo $uri;
   echo $user;
   echo json_encode($roles);
+  echo json_encode($routes);
   
   die();
 
