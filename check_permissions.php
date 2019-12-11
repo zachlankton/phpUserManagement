690b5e8b62a9b2ddf18fb1a28fedf3e4904544a6
zachlankton <zachlankton@gmail.com>
2019-12-10 22:57:45 -0500

    Update check_permissions.php

--- check_permissions.php
+++ check_permissions.php
@@ -8,22 +8,32 @@
   $roles = $account->getUserRoles();
   $req_type = $_SERVER['REQUEST_METHOD'];
   
+
+  // Find a Route that Matches the Requested URI
   $sth = $pdo->prepare("
     SELECT * FROM Application.`route_permissions` 
     WHERE request_type = :req_type AND :uri RLIKE route_regexp ");
   $sth->execute( array(':req_type' => $req_type, ':uri' => $uri) );
-  /* Fetch all of the remaining rows in the result set */
   $routes = $sth->fetchAll(PDO::FETCH_ASSOC);
 
+  // If there are no results then 404
   if (count($routes) == 0){
     http_response_code(404);
-    echo "Route Not Found";
+    echo "URI: ".$uri."<br>";
+    echo "Request Type: " . $req_type . "<br>";
+    echo "Route Not Found For Request Type.";
     die();
   }
   
-  if (!in_array($routes[0]['role'], $roles)) {
+  // Check if the User Has the Appropriate Role to Access The Requested Resource
+  // If not then issue 403 Forbidden!
+  $role = $routes[0]['role']
+  if (!in_array($role, $roles)) {
     http_response_code(403);
-    echo "User Forbidden to Access This Resource!";
+    echo "URI: ".$uri."<br>";
+    echo "Request Type: " . $req_type . "<br>";
+    echo "User Forbidden to Access This Resource! <br>";
+    echo $role . " Role Required ";
     die();
   }
 
