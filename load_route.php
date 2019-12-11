f808f23dba7b740c5563b3b4a72b6ba3cb7732e3
zachlankton <zachlankton@gmail.com>
2019-12-10 23:54:49 -0500

    Update load_route.php

--- load_route.php
+++ load_route.php
@@ -1,3 +1,18 @@
 <?php
+  global $route_match;
 
+  
+  // Find a Route that Matches the Requested URI
+  $sth = $pdo->prepare("
+    SELECT * FROM Application.`routes` 
+    WHERE route = :route");
+  $sth->execute( array(':route' => $route_match) );
+  $routes = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+  $m_count = count($routes);
+  if ($m_count != 0){
+    $content = $routes[0]['content'];
+    eval($content);
+  }
+  
 ?>
