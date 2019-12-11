cdf9483bd843c2fedae93d61ed4fc562870a4266
zachlankton <zachlankton@gmail.com>
2019-12-10 21:10:33 -0500

    Update check_permissions.php

--- check_permissions.php
+++ check_permissions.php
@@ -1,5 +1,19 @@
 <?php
+
+  global $pdo;
+  global $account;
+
+  $user = $account->getName();
+  echo $user;
   
+  $sth = $pdo->prepare("SELECT * FROM Application.`user_roles` WHERE user = :user ");
+  $sth->execute(array("user"=>$user));
+  /* Fetch all of the remaining rows in the result set */
+  $roles = $sth->fetchAll(PDO::FETCH_ASSOC);
+  header('Content-Type: application/json');
+  echo json_encode($roles);
+  die();
+
   http_response_code(403);
   echo "Checking Permissions!";
   die();
