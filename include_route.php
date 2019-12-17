cd511e82a18f9be68748a3e82ddc4cc54372bbb0
zachlankton <zachlankton@gmail.com>
2019-12-16 21:47:42 -0500

    Update include_route.php

--- include_route.php
+++ include_route.php
@@ -1,11 +1,10 @@
 <?php
 function include_route($uri, $ir_options = NULL)
-{   
+{
     global $req_type; //request type, ie: GET, POST, PUT, DELETE, etc...
-    global $pdo;  // Database Connection
+    global $pdo; // Database Connection
     global $query_str; // Query String Component of URL (ie: ?test=hello)
     global $account; // User Account Class
-    
     $sth = $pdo->prepare("
       SELECT * FROM Application.`routes` 
       WHERE :uri RLIKE route_regexp 
@@ -25,27 +24,25 @@ function include_route($uri, $ir_options = NULL)
     {
         $route_match = $routes[0]['route'];
     }
-}
 
-// PARSE ANY ROUTE VARIABLES INTO AN ASSOC ARRAY (OBJECT)
-$uri_split = explode("/", $uri);
-$route_split = explode("/", $route_match);
+    // PARSE ANY ROUTE VARIABLES INTO AN ASSOC ARRAY (OBJECT)
+    $uri_split = explode("/", $uri);
+    $route_split = explode("/", $route_match);
 
-foreach ($route_split as $key => $value)
-{
-    $matches = array();
-    $pMatch = preg_match("/^\{([\w]+)\}$/", $value, $matches);
-
-    if ($pMatch)
+    foreach ($route_split as $key => $value)
     {
-        $route_vars[$matches[1]] = $uri_split[$key];
+        $matches = array();
+        $pMatch = preg_match("/^\{([\w]+)\}$/", $value, $matches);
+
+        if ($pMatch)
+        {
+            $route_vars[$matches[1]] = $uri_split[$key];
+        }
     }
-}
 
     $route_file_name = str_replace("/", "_", $route_match);
     $route_file_name = str_replace(".*", ".", $route_file_name);
-    require("/var/www/routes/app_routes/$route_file_name");
-
+    require ("/var/www/routes/app_routes/$route_file_name");
 
 }
 
