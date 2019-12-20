0ab78fcbf8ae4e8bd5e7a37d0dda1022c6ceee58
zachlankton <zachlankton@gmail.com>
2019-12-20 18:14:02 -0500

    Update index2.php

--- html/index2.php
+++ html/index2.php
@@ -121,12 +121,40 @@ function find_routes(){
 		$pattern = $route['route_regexp'] ;
 		$f = preg_match("@$pattern@i", $uri);
 		if ($f == 1){
+			$route_file_name = str_replace("/", "_", $route['route']);
+    			$route['route_file_name'] = str_replace(".*", ".", $route_file_name);
+			$route['var_count'] = get_route_var_count($route['route']);
+			$route['route_length'] = get_route_length($route['route']);
 			return $route;
 		}
 	}
 
 	$found_routes = array_map('find', $routes);
 	echo json_encode($found_routes, JSON_PRETTY_PRINT);
+	die();
+	return found_routes;
+}
+
+function get_route_var_count($route){
+	$matches = array();
+	$pMatch = preg_match("/\{\w+\}/g", $value, $matches);
+	return count($matches)
+}
+
+function get_route_length($route){
+	$route_split = explode("/", $route);
+	$route_minus_vars = "";
+
+	foreach ($route_split as $key => $value) {
+		$matches = array();
+		$pMatch = preg_match("/^\{([\w]+)\}$/", $value, $matches);
+
+		if ($pMatch === 0){
+			$route_minus_vars .= $value;
+		}   
+	}
+	
+	return strlen($route_minus_vars);
 }
 
 function super_user_routes_match(){
@@ -189,55 +217,7 @@ function regular_user_routes_match(){
 	
 	
   
-  // Find a Route that Matches the Requested URI
-  $sth = $pdo->prepare("
-    SELECT * FROM Application.`route_permissions` 
-    WHERE request_type = :req_type AND :uri RLIKE route_regexp
-    ORDER BY `route_permissions`.`priority` ASC ");
-  $sth->execute( array(':req_type' => $req_type, ':uri' => $uri) );
-  $routes = $sth->fetchAll(PDO::FETCH_ASSOC);
-  // If there are no results then 404
-  $match_count = count($routes);
-  if ($match_count == 0){
-    http_response_code(404);
-    echo "URI: ".$uri."<br>";
-    echo "Request Type: " . $req_type . "<br>";
-    echo "Permissions For Route and Request Type Not Established.";
-    $failed = TRUE;
-    $err = "404 NOT FOUND";
-  }
   
-  // Check if the User Has the Appropriate Role to Access The Requested Resource
-  // If not then issue 403 Forbidden!
-  $role = $routes[0]['role'];
-  $match = $routes[0]['route'];
-  $route_match = $match;
-  if (!$failed && !in_array($role, $roles)) {
-    http_response_code(403);
-    echo "URI: ".$uri."<br>";
-    echo "Request Type: " . $req_type . "<br>";
-    echo "User Forbidden to Access This Resource! <br>";
-    echo $role . " Role Required ";
-    $failed = TRUE;
-    $err = "403 FORBIDDEN";
-  }
-  if ($failed) {
-    // Find a Route that Matches the Requested URI
-  $sth = $pdo->prepare("INSERT INTO `Application`.`access_error_log`(`error_type`, 
-  `user_name`, `uri_requested`, `request_type`, `role_required`, `route_match`, `match_count`) 
-  VALUES (:err,:user,:uri,:req_type,:role,:match,:count) ");
-    $values = array(
-      ':err'      => $err,
-      ':user'     => $user,
-      ':uri'      => $uri,
-      ':req_type' => $req_type,
-      ':role'     => $role,
-      ':match'    => $match,
-      ':count'    => $match_count
-    );
-  $sth->execute( $values );
-  die();
-  }
 }
 
 function load_routes(){
