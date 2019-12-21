6ea0403b7e3f14480bb9534ea4ad1d8910999b07
zachlankton <zachlankton@gmail.com>
2019-12-21 00:03:47 -0500

    Update adduser.php

--- routes/adduser.php
+++ routes/adduser.php
@@ -1,5 +1,5 @@
 <?php 
-	global $account;
+	global $pdo;
 
 	if (!isset($_POST['user'])){
 		require "../routes/adduser.html";
@@ -9,9 +9,13 @@
 	$user = $_POST["user"];
 	$pw = $_POST["pw"];
 	
+	$create_user_query = "CREATE USER '$user'@'localhost' IDENTIFIED BY '$pw'; ";
+	$grant_usage_query = "GRANT USAGE ON *.* TO '$user'@'localhost'; ";
+	$grant_user_query = "GRANT SELECT ON `Users`.* TO '$user'@'localhost'; ";
+	$grant_app_query = "GRANT SELECT ON `Application`.* TO '$user'@'localhost'; ";
 	try
 	{
-		$newId = $account->addAccount($user, $pw);
+		
 	}
 	catch (Exception $e)
 	{
