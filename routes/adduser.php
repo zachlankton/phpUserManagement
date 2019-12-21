5a7ef47fc7fdbc89502e586e3c44d40c42320330
zachlankton <zachlankton@gmail.com>
2019-12-21 00:31:57 -0500

    Update adduser.php

--- routes/adduser.php
+++ routes/adduser.php
@@ -10,11 +10,15 @@
 	$pw = $_POST["pw"];
 	
 	$create_user_query = "CREATE USER '$user'@'localhost' IDENTIFIED BY '$pw'; ";
-	$grant_usage_query = "GRANT USAGE ON *.* TO '$user'@'localhost'; ";
+	$insert_user_query = "INSERT INTO `Users`.`accounts`(`account_name`)VALUES('$user');"
 	$grant_user_query = "GRANT SELECT ON `Users`.* TO '$user'@'localhost'; ";
 	$grant_app_query = "GRANT SELECT ON `Application`.* TO '$user'@'localhost'; ";
 	try
 	{
+		$cuq = $pdo->query($create_user_query);
+		$iuq = $pdo->query($insert_user_query);
+		$guq = $pdo->query($grant_user_query);
+		$gaq = $pdo->query($grant_app_query);
 		
 	}
 	catch (Exception $e)
