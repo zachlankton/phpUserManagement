22797e38a43c50f2877aba7546c5b5649479c713
zachlankton <zachlankton@gmail.com>
2019-12-21 00:50:52 -0500

    Update adduser.php

--- routes/adduser.php
+++ routes/adduser.php
@@ -18,12 +18,14 @@
 	$insert_user_query = "INSERT INTO `Users`.`accounts`(`account_name`)VALUES('$user'); ";
 	$grant_user_query = "GRANT SELECT ON `Users`.* TO '$user'@'localhost'; ";
 	$grant_app_query = "GRANT SELECT ON `Application`.* TO '$user'@'localhost'; ";
+	$grant_err_log = "GRANT INSERT ON `Application`.`access_error_log` TO '$user'@'localhost'; ";
 	try
 	{
 		$cuq = $pdo->query($create_user_query);
 		$iuq = $pdo->query($insert_user_query);
 		$guq = $pdo->query($grant_user_query);
 		$gaq = $pdo->query($grant_app_query);
+		$gel = $pdo->query($grant_err_log);
 		
 	}
 	catch (Exception $e)
