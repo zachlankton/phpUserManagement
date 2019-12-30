a41266363b004cb173011f2ca6263c536c4729ce
zachlankton <zachlankton@gmail.com>
2019-12-30 13:13:56 -0500

    Update index.php

--- html/index.php
+++ html/index.php
@@ -450,7 +450,10 @@ function include_route($uri, $ir_options = NULL)
     }
     $route_file_name = str_replace("/", "_", $route_match);
     $route_file_name = str_replace(".*", ".", $route_file_name);
+	
+	echo "<a begin include_route href=\"$route_match\" style=\"display:none;\">Begin Include Route $route_match</a>";
     require ("/var/www/routes/app_routes/$route_file_name");
+	echo "<a end include_route href=\"$route_match\" style=\"display:none;\">End Include Route $route_match</a>";
 }
 
 
