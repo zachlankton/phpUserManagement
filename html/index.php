8fb647ebb169a21c56167a7318f0008fb1885133
zachlankton <zachlankton@gmail.com>
2020-01-05 00:35:52 -0500

    Update index.php

--- html/index.php
+++ html/index.php
@@ -526,7 +526,7 @@ function get_routes($uri){
 		
 		// IF USER IS AUTHENTICATED AND REQUESTING LOGOUT
 		if ($uri == '/logout'){
-			$results = $pdo->query("select id from information_schema.processlist where user='$user'; ");
+			//$results = $pdo->query("select id from information_schema.processlist where user='$user'; ");
 			$_SESSION = array();
 			session_destroy();
 			setcookie(session_name(),'',0,'/');
