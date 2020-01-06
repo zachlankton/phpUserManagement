6de553e2bea9416afd00bc260840595df67b587c
zachlankton <zachlankton@gmail.com>
2020-01-05 20:11:22 -0500

    Update adduser.php

--- routes/adduser.php
+++ routes/adduser.php
@@ -16,6 +16,7 @@
 	
 	$create_user_query = "CREATE USER '$user'@'localhost' IDENTIFIED WITH mysql_native_password BY '$pw'; ";
 	$insert_user_query = "INSERT INTO `Users`.`accounts`(`account_name`)VALUES('$user'); ";
+	$insert_user_role = "INSERT INTO `Application`.`user_roles`(`user`, `role`)VALUES('$user', 'User'); ";
 	$grant_user_query = "GRANT SELECT ON `Users`.* TO '$user'@'localhost'; ";
 	$grant_app_query = "GRANT SELECT ON `Application`.* TO '$user'@'localhost'; ";
 	$grant_err_log = "GRANT INSERT ON `Application`.`access_error_log` TO '$user'@'localhost'; ";
@@ -23,6 +24,7 @@
 	{
 		$cuq = $pdo->query($create_user_query);
 		$iuq = $pdo->query($insert_user_query);
+		$iur = $pdo->query($insert_user_role);
 		$guq = $pdo->query($grant_user_query);
 		$gaq = $pdo->query($grant_app_query);
 		$gel = $pdo->query($grant_err_log);
