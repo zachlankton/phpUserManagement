47a03039ce03d13d4694d56e894a658e17142551
zachlankton <zachlankton@gmail.com>
2020-02-01 11:06:20 -0500

    Update index.php

--- html/index.php
+++ html/index.php
@@ -674,7 +674,6 @@ function get_routes($uri){
 			// Set Content Type and Respond!
 			if ($return_arr){
 				header('Content-Type: text/html');
-				var_dump($output);
 				return json_decode($output);
 			}
 			die();
