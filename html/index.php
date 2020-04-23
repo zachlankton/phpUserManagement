9340bd06e3d2d61091cce7a4b7190fbab47efb05
zachlankton <zachlankton@gmail.com>
2020-04-23 00:03:47 -0400

    Update index.php

--- html/index.php
+++ html/index.php
@@ -289,7 +289,7 @@ use Nesk\Rialto\Data\JsFunction;
 			$puppet = new Puppeteer;
 			$browser = $puppet->launch();
 			$page = $browser->newPage();			
-			$page->setCookie( ["name"=>"PHPSESSID", "value"=>session_id(), "domain"=>"https://erp2.mmpmg.com" ] );
+			$page->setCookie( ["name"=>"PHPSESSID", "value"=>session_id(), "domain"=>"erp2.mmpmg.com" ] );
 			$page->goto("https://erp2.mmpmg.com" . $uri, ["waitUntil"=>"networkidle2"]);
 			//$page->waitFor(10000);
 			$html = $page->evaluate(JsFunction::createWithBody("
